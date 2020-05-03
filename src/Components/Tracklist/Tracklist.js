5a99cc0a7a94ace121edd85d3322fe7aa80bb547
phenk53 <phenk@hs-mittweida.de>
2020-05-03 16:26:32 +0200

    src files, components

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './TrackList.css';
+
+class Tracklist extends Component {
+
+    render () {
+
+        return (
+<div className="TrackList">
+    // You will add a map method that renders a set of Track components
+</div>
+        )
+    }
+}
+
+export default Tracklist;
