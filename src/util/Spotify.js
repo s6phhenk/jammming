7f6480acb2207350e1418dd974cfcb85f98b285c
phenk53 <phenk@hs-mittweida.de>
2020-05-14 10:49:03 +0200

    updated redirect Uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '12149a66ee094593afb87824562f886b';
-const redirectUri = 'http//:localhost:3000/';
+const redirectUri = 'http://jammming-project.surge.sh/';
 
 let accessToken; 
 
@@ -59,13 +59,13 @@ const Spotify = {
         // headers Variable entspricht dem headers Objekt für die fetch Methode s.u. 
         const headers = {Authorization: `Bearer ${accessToken}`}
         // wird die fetch Methode gebraucht --> jsonResponse.id wird gleich der userId gesetzt
-        let UserId;
+        let userId;
 
         return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => {
             response.json();
         }).then(jsonResponse => {
-            UserId = jsonResponse.id;
-            return fetch(`https://api.spotify.com/v1/users/${UserId}/playlists`, {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
                 method: 'POST',
                 headers: headers,
                 body: JSON.stringify({name: name})
